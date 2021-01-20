45bcad9a8d8fbfde8696c96a3934a2b1d57aca18
TAXO <a.i.cpvd3000@gmail.com>
2021-01-20 03:21:27 -0300

    First commit

---
+++ test.py
@@ -0,0 +1,37 @@
+#MAIN
+x=0
+nums = [1,3,4,3]
+print(len(nums))
+print(max(nums))
+print(min(nums))
+print(sum(nums))
+print(sum(nums)/len(nums))
+
+<<<<<<< HEAD
+#Al·lahu-àkbar
+=======
+#JESUS CHRIST
+#JESUS CHRIST IS THE NUMBER ONE
+>>>>>>> submaster
+#ERROR
+if __name__ == "__main__":
+    if x == 4:
+        while x >= 20:
+            print ("not")
+    print("****New exercise = Found the SMALLER number in a list*****")
+    lst=[3,4,6,1,3,6,8,3,1,-2]
+    z=max(lst)
+    for y in lst:
+        if z>y:
+            z=y
+            print("Min",z)
+        elif y==max(lst):
+            print("Max", y)
+        else:
+            print("  ",y)
+
+
+#ddddd
+    print("BIGGER NUMBER:", max(lst), " SMALLER NUMBER:",z)
+
+    pass
